6b822ed3bfa9424ba6e6abb244cfdd7f405f784f
Luma-Lima <38338127+Luma-Lima@users.noreply.github.com>
2020-05-28 21:55:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+var receita = {
+	titulo: 'brigadeiro',
+	porcao: '40 unidades',
+	ingredientes: ['manteinga','toddy','leite condensado', 'leite']
+}
+
+console.log(receita.titulo);
+console.log(receita.porcao);
+console.log(receita.ingredientes);
+
+
+var livro = [
+{
+  titulo: 'A menina que roubava livros',
+  autor: 'num lembro',
+  livroLido: true
+},
+
+{
+  titulo: 'A menina que roubava livros',
+  autor: 'num lembro',
+  livroLido: true
+},
+
+{
+  titulo: 'A menina que roubava livros',
+  autor: 'num lembro',
+  livroLido: true
+},
+
+{
+  titulo: 'A menina que roubava livros',
+  autor: 'num lembro',
+  livroLido: true
+}
+];
+for (i=0; i<livro.length; i++){
+	console.log(livro[i]);
+}
+
